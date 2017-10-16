50011cb8a515ea8b49ff3c1692bfd9a75700bba4
kaneyama <kaneyama@turbine.co.jp>
2017-10-16 16:09:34 +0900

    panorama error

--- src/assets/scripts/googlemapapi_practice.js
+++ src/assets/scripts/googlemapapi_practice.js
@@ -1,16 +1,51 @@
-// 初期化
-initMap(38.126434, 136.969707, 5);
+(function ($) {
 
-//関数の定義（マップのイニシャライズ）
-function initMap(lat, lng, zoom) {
+  'use strict';
 
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: zoom,
-    center: {
-      lat: lat,
-      lng: lng
+  // 初期化
+  initMap(38.126434, 136.969707, 5);
+
+  //関数の定義（マップのイニシャライズ）
+  function initMap(lat, lng, zoom) {
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: zoom,
+      center: {
+        lat: lat,
+        lng: lng
+      }
+    });
+  };
+
+  $.getJSON("markers.json", function (data) {
+    console.log('json Data:' + data.markers[0].place);
+    for (var i = 0; i < data.markers.length; i++) {
+      var name = data.markers[i].place;
+      var latlng = new google.maps.LatLng(data.markers[i].lat, data.markers[i].lng);
+      var icons = data.markers[i].img;
+      createMarker(name, latlng, icons, map);
+      console.log('name:'+ name);
+      console.log('latlng:'+ latlng);
+      console.log('icons:'+ icons);
     }
   });
+
+
+  /*
+
+    $.getJSON("markers.json", function (markers) {
+      for (var i in markers = 0; i < markers.length; i++) {
+        var name = markers[i].place;
+        var latlng = new google.maps.LatLng(markers[i].lat, markers[i].lng);
+        var icons = markers[i].img;
+        createMarker(name, latlng, icons, map)
+      }
+    });
+  });
+
+  */
+
+  /*
 
   var markers = [
     ['飛騨', 36.239905, 137.186486, '../assets/images/google-map-api/mapiconsstation.png'],
@@ -26,41 +61,47 @@ function initMap(lat, lng, zoom) {
     createMarker(name, latlng, icons, map)
   }
 };
+*/
 
-// マーカーを置く
-function createMarker(name, latlng, icons, map) {
-  var marker = new google.maps.Marker({
-    position: latlng,
-    icon: icons,
-    map: map
-  });
-}
+  // マーカーを置く
+  function createMarker(name, latlng, icons, map) {
+    var marker = new google.maps.Marker({
+      position: latlng,
+      icon: icons,
+      map: map
+    });
+  }
 
 
-// フォームから座標情報を受け取って行う処理の実行
-document.addEventListener('click', function (e) {
-  if (e.target.className === 'search-button') {
-    var geo = document.getElementById("geo-search").value;
-    // フォームから住所を受け取って行う処理の実行
 
-    var geocoder = new google.maps.Geocoder();
-    geocoder.geocode({
-        'address': geo
-      },
-      function (results, status) {
-        // 取得できたら
-        if (status == google.maps.GeocoderStatus.OK) {
-          // 緯度経度を変数へ格納
-          var debug = results[0];
-          var lat = results[0].geometry.bounds.f.b;
-          var lng = results[0].geometry.bounds.b.b;
-          // 取得した緯度経度でGoogleMap表示
-          initMap(lat, lng, 11)
-          console.log(debug);
-        } else {
-          console.log('error:GeocoderStatus = No');
+
+
+
+  // フォームから座標情報を受け取って行う処理の実行
+  document.addEventListener('click', function (e) {
+    if (e.target.className === 'search-button') {
+      var geo = document.getElementById("geo-search").value;
+      // フォームから住所を受け取って行う処理の実行
+
+      var geocoder = new google.maps.Geocoder();
+      geocoder.geocode({
+          'address': geo
+        },
+        function (results, status) {
+          // 取得できたら
+          if (status == google.maps.GeocoderStatus.OK) {
+            // 緯度経度を変数へ格納
+            var debug = results[0];
+            var lat = results[0].geometry.bounds.f.b;
+            var lng = results[0].geometry.bounds.b.b;
+            // 取得した緯度経度でGoogleMap表示
+            initMap(lat, lng, 11)
+            console.log(debug);
+          } else {
+            console.log('error:GeocoderStatus = No');
+          }
         }
-      }
-    )
-  };
-});
+      )
+    };
+  });
+})(jQuery)
